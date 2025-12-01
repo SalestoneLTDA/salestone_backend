ed110ff1db3dda945e14bf2992227c5b05251495
VitorVieira <vitorhvvieira@gmail.com>
2025-12-01 20:19:28 -0300

    Estrutura do projeto

---
+++ src/main/java/com/salestonetech/salestone/SalestoneApplication.java
@@ -0,0 +1,13 @@
+package com.salestonetech.salestone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalestoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalestoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salestonetech/salestone/SalestoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salestonetech.salestone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalestoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
